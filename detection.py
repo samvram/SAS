2cc8f3b6f21b10d9e88e2a106e2f7284a8282e17
Samvram Sahu <samvram.iist@gmail.com>
2018-04-03 12:03:38 +0530

    Without Rectangles

--- detection.py
+++ detection.py
@@ -3,7 +3,8 @@ import os
 from preproc import stretch
 from PIL import Image
 
-face_cascade = cv.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+face_cascade_alt2 = cv.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 # face_cascade.load('haarcascade_frontalface_default.xml')
 # print(face_cascade)
 
@@ -15,15 +16,21 @@ for file in os.listdir('images'):
     x['RGB'] = cv.imread(file_path)
     x['GRAY'] = cv.cvtColor(x['RGB'], cv.COLOR_BGR2GRAY)
     x['PAR'] = x['GRAY']
+    x['RGB'] = x['GRAY']
     images.append(x)
 
 print('Length of images : %s'%str(len(images)))
 list_of_faces = []
 for image in images:
-    faces = face_cascade.detectMultiScale(image['GRAY'], 1.3, 5)
+    faces = face_cascade_alt2.detectMultiScale(image['GRAY'], 1.3, 5)
     for (x,y,w,h) in faces:
-        image['RGB'] = cv.rectangle(image['GRAY'],(x,y),(x+w,y+h),(255,0,0), 8)
-        list_of_faces.append(stretch(image['PAR'][y:y+h, x:x+w]))
+        # cv.rectangle(image['GRAY'],(x,y),(x+w,y+h),(255,0,0), 8)
+        list_of_faces.append(stretch(image['RGB'][y:y+h, x:x+w]))
+    faces_2 = face_cascade.detectMultiScale(image['GRAY'], 1.3, 5)
+    for (x,y,w,h) in faces_2:
+        if (x,y,w,h) not in faces:
+            # cv.rectangle(image['GRAY'], (x, y), (x + w, y + h), (255, 0, 0), 8)
+            list_of_faces.append(stretch(image['RGB'][y:y + h, x:x + w]))
 
 i=0
 for file in os.listdir('processed_images'):
@@ -42,6 +49,9 @@ for face in list_of_faces:
     i = i + 1
     # print('faces')
     cv.imwrite(os.path.join('processed_faces','face_detailed_'+str(i)+'.jpg'), face)
+
+for file in os.listdir('processed_face_resize'):
+    os.remove(os.path.join('processed_face_resize',file))
 
 for file in os.listdir('processed_faces'):
     img = Image.open(os.path.join('processed_faces',file))
